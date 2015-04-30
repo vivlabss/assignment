b68fefefc573d83daf7c53109079d893028e27e4
이영주/통합ICTO팀 <yzoo34@naver.com>
2015-04-30 16:10:43 +0900

    day1. 기초적인 클래스 예시 코드

---
+++ A03_Employee.java
@@ -0,0 +1,15 @@
+package day1;
+
+public class A03_Employee {
+
+	// [ 객체지향의 핵심, 클래스 ]
+	// 한 객체에 대한 다양한 타입의 정보를 하나의 묶음으로 관리할 수 있다.
+		
+	String name;
+	int age;
+	String position;
+	int salary;
+	double comm;
+	String hire;
+
+}
